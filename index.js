a06f2ea4582c785350ac0204e485132cbccade35
KatyRosli <96566968+KatyRosli@users.noreply.github.com>
2022-01-13 21:29:49 +0100

    fetch &display the hourly forecast

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ function showSearchFormValues(event) {
   event.preventDefault();
   let cityElement = document.querySelector("#city");
   let cityInput = document.querySelector("#city-input");
+  document.querySelector("#display-temperature-letter").innerHTML = "C";
+  document.querySelector("#view-temperature-letter").innerHTML = "F";
   cityElement.innerHTML = cityInput.value;
 
   let apiKey = "313875bf8edc10d6e458db37d82896b3";
@@ -31,21 +33,20 @@ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", showSearchFormValues);
 
 // from celsius to fahrenheit
-
 function convertTemperature() {
-  let temperatureLetter = document.querySelector("#display-letter");
   let displayValue = document.querySelector("#display-value");
+  let displayTemperatureLetter = document.querySelector(
+    "#display-temperature-letter"
+  );
   let temperatureButtonLetter = "";
-  if (temperatureLetter.innerHTML.includes("C")) {
-    displayValue.innerHTML = `${Math.round(displayValue.innerHTML * 1.8 + 32)}`;
+  if (displayTemperatureLetter.innerHTML.includes("C")) {
+    displayValue.innerHTML = Math.round(displayValue.innerHTML * 1.8 + 32);
+    displayTemperatureLetter.innerHTML = "F";
     temperatureButtonLetter = "C";
-    temperatureLetter.innerHTML = "F";
   } else {
-    displayValue.innerHTML = `${Math.round(
-      (displayValue.innerHTML - 32) / 1.8
-    )}`;
+    displayValue.innerHTML = Math.round((displayValue.innerHTML - 32) / 1.8);
+    displayTemperatureLetter.innerHTML = "C";
     temperatureButtonLetter = "F";
-    temperatureLetter.innerHTML = "C";
   }
   document.querySelector("#view-temperature-letter").innerHTML =
     temperatureButtonLetter;
@@ -71,18 +72,19 @@ function showTemperature(response) {
 
   displayValueElement.innerHTML = temperature;
   cityElement.innerHTML = response.data.name;
-  humidityElement.innerHTML = ` ${response.data.main.humidity}`;
+  humidityElement.innerHTML = `${response.data.main.humidity}`;
   windElement.innerHTML = ` ${Math.round(response.data.wind.speed)}`;
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  console.log("getCityForecast: " + response.data.name);
+  // show city forecast
+  getCityForecast(response.data.name);
 }
 
 function showPosition(position) {
-  console.log(position.coords.latitude);
-  console.log(position.coords.longitude);
   let apiKey = "313875bf8edc10d6e458db37d82896b3";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
@@ -92,9 +94,51 @@ function showPosition(position) {
 }
 
 function getCurrentLocation() {
-  console.log("CURRENT LOCATION");
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
 let locationButton = document.querySelector("#current-location");
 locationButton.addEventListener("click", getCurrentLocation);
+
+getCurrentLocation();
+
+function getCityForecast(city) {
+  let apiKey = "313875bf8edc10d6e458db37d82896b3";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  console.log("getCityForecast:" + url);
+  axios.get(url).then((resp) => {
+    let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${resp.data.coord.lat}&lon=${resp.data.coord.lon}&units=metric&appid=${apiKey}`;
+    axios.get(url2).then((resp) => {
+      showForecast(resp);
+    });
+  });
+}
+
+// display 6 forecast
+function showForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastText = forecastElement.innerHTML;
+  let index = 0;
+  response.data.hourly.forEach((item) => {
+    if (index < 6) {
+      let date = new Date(item.dt * 1000);
+      let hours = date.getHours();
+
+      if (index == 0) {
+        hours = "now";
+      } else if (hours < 10) {
+        hours = `0${hours}`;
+      }
+
+      forecastText =
+        forecastText +
+        `<div class="col-2">${hours}<br />
+      <img src="http://openweathermap.org/img/wn/${
+        item.weather[0]["icon"]
+      }@2x.png" class="weathericon" id="icon"/>
+      ${Math.round(item.temp)}°</div>`;
+    }
+    index++;
+  });
+  forecastElement.innerHTML = forecastText;
+}
